df8341d79fab42f433fa0c338bd132aebcc88193
Osekobo <collinsboseko2005@gmail.com>
2025-10-25 19:26:47 +0300

    css2

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-db = SQLAlchemy()
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+# db = SQLAlchemy()
 
-Class Users(db.model)
- __tablename__ = "Users"
-id = db.column(db) 
+# Class Users(db.model)
+#  __tablename__ = "Users"
+# id = db.column(db) 
